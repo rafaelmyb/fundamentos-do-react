8891be3fdab6282e85010008c42a642c8b455c89
Rafael Yamada <rafaelyamada321@gmail.com>
2021-09-03 17:13:56 -0400

    Implementing the exchange of theme

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.footer`
-  background: #111;
+  background: ${({ theme }) => theme.footerBackgroundColor};
   height: 70px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 24px;
+  padding: 0 ${({ theme }) => theme.spacing.large}px;
   border-radius: 10px;
-  margin-top: 24px;
+  margin-top: ${({ theme }) => theme.spacing.large}px;
 
   button {
     cursor: pointer;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,16 +1,16 @@
-import styled from 'styled-components';
+import styled from 'styled-components'; 
 
 export const Container = styled.header`
-  background: #111;
+  background: ${({ theme }) => theme.headerBackgroundColor};
   height: 100px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 24px;
-  border-radius: 10px;
+  padding: 0 ${({ theme }) => theme.spacing.large}px;
+  border-radius: ${({ theme }) => theme.borderRadius};
 
   button {
-    font-size: 16px;
+    font-size: ${({ theme }) => theme.spacing.medium}px;
     cursor: pointer;
     background: transparent;
     border: none;
